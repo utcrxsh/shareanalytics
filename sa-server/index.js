5d140f0da0d5c9d182fe0711053a9682ec49c0c1
Utkarsh <utkarshnautiyal321@gmail.com>
2023-09-18 17:09:12 +0530

    Update index.js

--- sa-server/index.js
+++ sa-server/index.js
@@ -57,7 +57,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // setting up routes
 
-app.use("/sa-server/auth", authroute);
+app.use("/auth", authroute);
 
 const PORT = process.env.PORT || 5000;
 
